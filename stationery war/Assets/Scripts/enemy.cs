4b397a355182097a09974c212ff4e523a202c1a6
puddingdoggy <123567449+puddingdoggy@users.noreply.github.com>
2025-06-28 21:38:02 +0800

    player

--- stationery war/Assets/Scripts/enemy.cs
+++ stationery war/Assets/Scripts/enemy.cs
@@ -14,12 +14,13 @@ enum EnemyState
 
 public class enemy : MonoBehaviour
 {
+    EnemyState enemyState = EnemyState.Move;
     private Rigidbody2D rb;
-    public float moveSpeed = 2;
     private Animator anim;
 
-    public int atkValue = 30;
-    public float atkDuration = 2;
+    public int atkValue = 0;
+    public float moveSpeed = 0;
+    public float atkDuration = 0;
     private float atkTimer = 0;
 
     public int HP = 100;
@@ -29,16 +30,57 @@ public class enemy : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        MoveUpdate();
+        switch (enemyState)
+        {
+            case EnemyState.Move:
+                MoveUpdate();
+                break;
+            case EnemyState.attack:
+                AttactUpdate();
+                break;
+            case EnemyState.Die:
+                break;
+            default:
+                break;
+        }
     }
 
     void MoveUpdate()
     {
         rb.MovePosition(rb.position + Vector2.left * moveSpeed * Time.deltaTime);
     }
+
+    void AttactUpdate()
+    {
+        
+    }
+
+
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" || collision.tag == "Playerhouse")
+        {
+            //anim.SetBool("IsAttacking", true);    //     ¶¯»­
+            enemyState = EnemyState.attack;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            //anim.SetBool("IsAttacking", false);
+            enemyState = EnemyState.Move;
+        }
+    }
+
 }
