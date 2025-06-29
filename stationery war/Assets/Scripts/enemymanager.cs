905697f93dced026fbd5287519c8846b5e1cd1cb
puddingdoggy <123567449+puddingdoggy@users.noreply.github.com>
2025-06-29 21:56:43 +0800

    enemycreate

---
+++ stationery war/Assets/Scripts/enemymanager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum SpawnState
+{
+    NotStart,
+    Spawning,
+    End
+}
+
+public class enemymanager : MonoBehaviour
+{
+    public static enemymanager Instance { get; private set; }
+    private SpawnState spawnState = SpawnState.NotStart;
+
+    public Transform[] spawnPointList;
+    public GameObject penPrefab;
+    public GameObject rubPrefab;
+
+    private List<enemy> zombieList = new List<enemy>();
+
+    void Start()
+    {
+        StartSpawn();
+    }
+
+
+    public void StartSpawn()
+    {
+        spawnState = SpawnState.Spawning;
+        SpawnZombie();
+    }
+    IEnumerator SpawnZombie()
+    {
+
+        yield return new WaitForSeconds(3);
+        //µÚÒ»²¨ 5
+        for (int i = 0; i < 30; i++)
+        {
+            SpawnARandomZombie();
+            yield return new WaitForSeconds(5);
+        }
+
+    }
+    private void SpawnARandomZombie()
+    {
+        int index = Random.Range(0,10);
+        if (index <= 4)
+        { 
+            GameObject.Instantiate(penPrefab, spawnPointList[0].position, Quaternion.identity); 
+        }
+        else if (index >= 5) 
+        {
+            GameObject.Instantiate(rubPrefab, spawnPointList[0].position, Quaternion.identity);
+        }
+    }
+
+}
